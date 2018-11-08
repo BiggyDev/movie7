4f1c4d14ff67c4924738b1f9da117b2dd40d8497
Yamna Melki <melkiyamna@protonmail.com>
2018-11-08 10:23:11 +0100

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+
+    </body>
+</html>
