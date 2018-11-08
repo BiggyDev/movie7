922a1b09448caff74fe18089a093cb634d2473c6
unknown <jeanluc.oudjoudi@gmail.com>
2018-11-08 11:26:54 +0100

    index

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,24 @@
 
+
+</div>
+
     <footer>
 
+    <div class="rs">
+      <h3>Suivez-nous</h3>
+      <a href="https://fr-fr.facebook.com/" target="_blank" ><img src="inc/fb.png" alt="fb"></a>
+      <a href="https://twitter.com/?lang=fr" target="_blank"><img src="inc/fb.png" alt="twitter"</a>
+    </div>
 
-
-
-
-
-
-
-
+    <div class="contact">
+      <nav>
+        <ul>
+          <li>Contactez-nous</li>
+          <li>Qui sommes-nous</li>
+          <li>Conditions d'utilisation</li>
+        </ul>
+      </nav>
+    </div>
 
 
 
