d30c6724e0200a5b3c8c1100aae45073170d1f7f
BiggyDev <vincent.carpentier111@gmail.com>
2018-11-08 15:43:24 +0100

    index

--- inc/functions.php
+++ inc/functions.php
@@ -5,3 +5,21 @@ function reloadImage($movie)
 <img src="posters/<?php echo $movie['id']; ?>.jpg" alt="<?php echo $movie['title']; ?>">
 <?php
 }
+
+
+
+function affichageCategories()
+{
+  foreach ($genres as $genre) {
+    $g = $genre['genres'];
+    $explodes = explode(',',$g);
+    foreach ($explodes as $explode) {
+      $ex = trim($explode);
+      if(!in_array($ex,$tableau)) {
+        if(!empty($ex)) {
+          $tableau[] = $ex;
+        }
+      }
+    }
+  }
+}
--- index.php
+++ index.php
@@ -1,41 +1,67 @@
 <?php include('inc/pdo.php');
 include('inc/functions.php');
 
-// Requête à la base de données affichage aléatoire
-$sql = "SELECT * FROM movies_full ORDER BY rand() LIMIT 50";
-$query = $pdo -> prepare($sql);
-$query -> execute();
-$movies = $query -> fetchAll();
-// Fin de la requête
+if(!empty($_POST['submitted'])) {
+  $variable = $_POST['genre'];
+  // echo '<pre>';
+  // print_r($variable);
+  // echo '</pre>';
 
-// Requête à la base de données filtres
-$sql = "SELECT * FROM movies_full WHERE 1=1
-AND genres LIKE '%Western%'
-AND genres LIKE '%Action%'
-AND genres LIKE '%Adventure%'
-AND genres LIKE '%Comedy%'
-AND genres LIKE '%Crime%'
-AND genres LIKE '%Drama%'
-AND genres LIKE '%Thriller%'
-AND genres LIKE '%Romance%'
-AND genres LIKE '%War%'
-AND genres LIKE '%Musical%'
-AND genres LIKE '%History%'
-AND genres LIKE '%Sci-Fi%'
-AND genres LIKE '%Animation%'
-AND genres LIKE '%Family%'
-AND genres LIKE '%Short%'";
+  // Requête à la base de données filtres
+   $sql = "SELECT * FROM movies_full WHERE 1=1 ";
+   foreach ($variable as $key => $v) {
+   $sql .= "AND genres LIKE '%" . $v . "%'";
+   }
+   $query = $pdo -> prepare($sql);
+   $query -> execute();
+   $genres = $query -> fetch();
+  // Fin de la requête
+
+  $sql = "SELECT * FROM movies_full WHERE genres = :genres";
+  $query = $pdo -> prepare($sql);
+  $query -> bindValue(':genres', $genres, PDO::PARAM_STR);
+  $query -> execute();
+  $filter = $query -> fetch();
+  echo '<pre>';
+  print_r($filter);
+  echo '</pre>';
+
+
+} else {
+  // Requête à la base de données affichage aléatoire
+  $sql = "SELECT * FROM movies_full ORDER BY rand() LIMIT 50";
+  $query = $pdo -> prepare($sql);
+  $query -> execute();
+  $movies = $query -> fetchAll();
+  // Fin de la requête
+}
+
+//Requête BDD
+$sql = "SELECT genres FROM movies_full ORDER BY genres";
 $query = $pdo -> prepare($sql);
 $query -> execute();
-$genres = $query -> fetch();
-// Fin de la requête
+$genres = $query -> fetchAll();
+
+$tableau = array();
+foreach ($genres as $genre) {
+  $g = $genre['genres'];
+  $explodes = explode(',',$g);
+  foreach ($explodes as $explode) {
+    $ex = trim($explode);
+    if(!in_array($ex,$tableau)) {
+      if(!empty($ex)) {
+        $tableau[] = $ex;
+      }
+    }
+  }
+}
 
 
 ?>
 <?php include('inc/header.php'); ?>
 <form class="filtergenres" action="" method="post">
-  <?php foreach ($genres as $genre) { ?>
-    <input type="checkbox" name="genre" value="<?php echo $movies['genre']; ?>">
+  <?php foreach ($tableau as $tab) { ?>
+    <label for=""><input type="checkbox" name="genre[]" value="<?= $tab ?>"><?php echo $tab; ?></label>
   <?php } ?>
   <input type="submit" name="submitted" value="Filtrer">
 </form>
@@ -51,7 +77,8 @@ $genres = $query -> fetch();
 //       Boucle affichage aléatoire films
   foreach ($movies as $movie) { ?>
     <div class="filmtitle">
-      <a href="details.php?id=<?php echo $movie['id']; ?>"><?php reloadImage($movie); ?>
+      <a href="details.php?id=<?php echo $movie['id']; ?>">
+      <?php reloadImage($movie);?>
     </div>
   <?php }
 //        Fin boucle
