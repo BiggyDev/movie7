873d5bc128da43a2dc786edab81b022d09f2d074
unknown <jeanluc.oudjoudi@gmail.com>
2018-11-08 12:56:23 +0100

    index

--- inc/footer.php
+++ inc/footer.php
@@ -4,23 +4,30 @@
 
     <footer>
 
+
+      <div class="contact">
+        <nav>
+          <ul>
+            <li>Contactez-nous</li>
+            <li>Qui sommes-nous</li>
+            <li>Conditions d'utilisation</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="clear"></div>
+
+
     <div class="rs">
       <h3>Suivez-nous</h3>
-      <a href="https://fr-fr.facebook.com/" target="_blank" ><img src="inc/fb.png" alt="fb"></a>
-      <a href="https://twitter.com/?lang=fr" target="_blank"><img src="inc/fb.png" alt="twitter"></a>
-      <a href="https://www.instagram.com/?hl=fr" target="_blank"><img src="inc/fb.png" alt="instagram"></a>
+
+      <a target="blank" href="https://fr-fr.facebook.com/" ><img src="imgs/social-icons.png" alt="fb"></a>
+      <a href="https://twitter.com/?lang=fr" target="blank"><img src="inc/fb.png" alt="twitter"></a>
+      <a href="https://www.instagram.com/?hl=fr" target="blank"><img src="inc/fb.png" alt="instagram"></a>
 
     </div>
 
-    <div class="contact">
-      <nav>
-        <ul>
-          <li>Contactez-nous</li>
-          <li>Qui sommes-nous</li>
-          <li>Conditions d'utilisation</li>
-        </ul>
-      </nav>
-    </div>
+
 
 
 
