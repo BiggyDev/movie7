8383d480ab0af21a6f6f5852ccf7e6b35cdf0269
Yamna Melki <melkiyamna@protonmail.com>
2018-11-08 11:19:53 +0100

    header

--- inc/header.php
+++ inc/header.php
@@ -1,1 +1,22 @@
-<?php
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="asset/style.css">
+        <title>Title</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="">Accueil</a></li>
+                    <li><a href="">Inscription</a></li>
+                    <li><a href="">Connexion</a></li>
+                    <li><a href="">Deconnexion</a></li>
+                    <li><a href="" class="account">Mon compte</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="wrap">
+
